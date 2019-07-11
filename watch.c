63bd637c3189aa3314ef6f89d7f63eee7f2c53cd
JeonMoonSu <munsoo3851@naver.com>
2019-07-11 10:47:43 +0900

    blackbox

---
+++ watch.c
@@ -0,0 +1,77 @@
+//Parent Process
+//This Process call a child process(blackbox call)
+//After fork() execute execlp()
+//sigHandler()
+//signal()
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<errno.h>
+#include<string.h>
+#include<sys/types.h>
+#include<wait.h>
+#include<sys/time.h>
+
+struct timeval UTCtime_s, UTCtime_e, UTCtime_r;
+int sec;
+int first = 0;
+
+void disp_runtime(struct timeval UTCtime_s, struct timeval UTCtime_e)
+{
+	struct timeval UTCtime_r;
+	if((UTCtime_e.tv_usec- UTCtime_s.tv_usec)<0)
+	{
+		UTCtime_r.tv_sec  = UTCtime_e.tv_sec - UTCtime_s.tv_sec - 1;
+		UTCtime_r.tv_usec = 1000000 - UTCtime_e.tv_usec - UTCtime_s.tv_usec;
+	}
+	else
+	{
+		UTCtime_r.tv_sec = UTCtime_e.tv_sec - UTCtime_s.tv_sec;
+		UTCtime_r.tv_usec = UTCtime_e.tv_usec - UTCtime_s.tv_usec;
+	}
+	printf("runtime : %ld sec %ld\n", UTCtime_r.tv_sec, UTCtime_r.tv_usec);
+}
+
+void sigHandler(int sig)
+{
+	if(first)
+	{
+		gettimeofday(&UTCtime_e,NULL);
+		printf("Black box is still alive!\n ");
+		disp_runtime(UTCtime_s,UTCtime_e);
+	}
+	else
+	{
+		printf("Black box recording start!\n");
+		first = 1;
+	}	
+	
+	gettimeofday(&UTCtime_s,NULL);
+}
+
+void main()
+{
+	pid_t pid;
+
+	int status;	
+	int ret;
+
+	signal(SIGUSR1,sigHandler);
+
+	pid = fork();
+
+	switch(pid)
+	{
+		case	-1 : perror("fork failed"); exit(0);
+		case 	 0 : ret = execlp("./blackbox","blackbox",NULL);
+					 fprintf(stderr,"execlp error:%s\n",strerror(errno));
+					 exit(0);
+		default : 	
+					 break;
+	}
+	pid = wait(&status);
+	printf("exited pid = %d\n",pid);
+}
+
